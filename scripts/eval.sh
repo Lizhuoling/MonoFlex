efb5c81fd055ccd6fb342b1cf2e686a642065358
twilightLZL <1105069197@qq.com>
2021-08-11 20:41:18 +0800

    2021.8.11

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,4 +1,6 @@
-CUDA_VISIBLE_DEVICES=3 python plain_train_net.py \
+exp_id=official
+
+CUDA_VISIBLE_DEVICES=3 python -B plain_train_net.py \
     --config runs/monoflex.yaml \
-    --ckpt ./output/backbone_scratch/model_moderate_best_soft.pth  \
+    --ckpt ./output/$exp_id/model_moderate_best_soft.pth  \
     --eval \
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,8 @@
-exp_id=debug
+exp_id=official_seed26_2
 
-CUDA_VISIBLE_DEVICES=0 python plain_train_net.py \
+CUDA_VISIBLE_DEVICES=3 python -B plain_train_net.py \
 --batch_size 8 \
 --config runs/monoflex.yaml \
 --output output/$exp_id \
+--seed 26 \
 #--scratch_backbone \
